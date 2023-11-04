b0aba64490c7e3cf731fe798025386d3ee13ddf7
Andrew Hanshaw <drew@andrewhanshaw.com>
2023-11-04 16:24:58 -0500

    Initial Commit

---
+++ PassKeepr/PassKeeprApp.swift
@@ -0,0 +1,17 @@
+//
+//  PassKeeprApp.swift
+//  PassKeepr
+//
+//  Created by Drew on 11/4/23.
+//
+
+import SwiftUI
+
+@main
+struct PassKeeprApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
