9a6dbcda7666ce3d5e266ed0a4f8bbc4bb1930ed
Andrew Hanshaw <andrewhanshaw@2022-MacBook-Air.local>
2023-11-12 13:10:35 -0600

    Make ListSection dynamic

--- PassKeepr/ContentView.swift
+++ PassKeepr/ContentView.swift
@@ -3,11 +3,23 @@ import SwiftUI
 struct ContentView: View {
     @Environment(ModelData.self) var modelData
     
+    // TODO: Make this dynamic
+    let maxType = 3
+    
+    var filteredList1: [ListItem] {
+        modelData.listItems.filter { $0.type == 1 }
+    }
+    
+    var filteredList2: [ListItem] {
+        modelData.listItems.filter { $0.type == 2 }
+    }
+    
     var body: some View {
         NavigationView{
             VStack {
                 List {
-                    ListSection(modelData: _modelData)
+                    ListSection(list: filteredList1)
+                    ListSection(list: filteredList2)
                     .navigationTitle("All Passes")
                 }
                 HStack {
--- PassKeepr/Model/ListItem.swift
+++ PassKeepr/Model/ListItem.swift
@@ -10,4 +10,11 @@ import Foundation
 struct ListItem: Codable, Hashable, Identifiable {
     var id: Int
     var name: String
+    var type: Int
+    
+    init() {
+        self.id = 1000
+        self.type = 0 // Set default type value
+        self.name = "Default Name" // Set default name value
+    }
 }
