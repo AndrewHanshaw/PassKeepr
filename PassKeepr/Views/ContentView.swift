01c3e46222a0e8ca43e82a0abc7b6344047c1d87
Andrew Hanshaw <andrewhanshaw@2022-MacBook-Air.local>
2024-01-08 20:55:49 -0600

    Make AddPass display as a sheet

--- PassKeepr/Views/ContentView.swift
+++ PassKeepr/Views/ContentView.swift
@@ -2,7 +2,9 @@ import SwiftUI
 
 struct ContentView: View {
     @Environment(ModelData.self) var modelData
-    
+
+    @State var shouldPresentSheet = false
+
     var filteredList1: [ListItem] {
         modelData.listItems.filter { $0.passType == PassType.barcodePass }
     }
@@ -21,12 +23,19 @@ struct ContentView: View {
                 }
                 HStack {
                     Spacer()
-                    
-                    NavigationLink(destination: AddPass()) {
-                        Image(systemName: "plus.circle.fill")
-                            .font(.largeTitle)
-                            .padding()
-                            .foregroundColor(.accentColor)
+                    Button(role: .none,
+                           action: {shouldPresentSheet.toggle()},
+                           label: {
+                            Image(systemName:"plus.circle.fill")
+                                .resizable()
+                                .scaledToFit()
+                                .frame(width: 50)
+                           }
+                    )
+                    .labelStyle(.iconOnly)
+                    .padding([.trailing], 33)
+                    .sheet(isPresented: $shouldPresentSheet) {
+                        AddPass(isSheetPresented: $shouldPresentSheet)
                     }
                 }
             }
