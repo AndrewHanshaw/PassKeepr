2697f40c54914c2a4dd7312c34594eb071b3eaa3
Andrew Hanshaw <andrewhanshaw@2022-MacBook-Air.local>
2024-01-15 11:32:10 -0600

    Add settings view

---
+++ PassKeepr/Views/Settings.swift
@@ -0,0 +1,43 @@
+//
+//  Settings.swift
+//  PassKeepr
+//
+//  Created by Andrew Hanshaw on 1/6/24.
+//
+
+import SwiftUI
+
+struct Settings: View {
+    let filename = "PassKeeprData.json"
+
+    @State private var isDocumentPickerPresented: Bool = false
+
+    var body: some View {
+        Form(){
+            Section {
+                Button ("Delete All Passes", role: .destructive) {
+                    deleteAllItems(filename:filename)
+                }
+                Button ("Delete Data File", role: .destructive) {
+                    deleteDataFile(filename:filename)
+                }
+                let iconView = AppIcon().frame(width: 1024, height: 1024)
+                let cgImage = ImageRenderer(content: iconView).cgImage!
+                let uiimage = UIImage(cgImage: cgImage)
+                Button ("Save Icon image") {
+                    self.isDocumentPickerPresented.toggle()
+                }
+                .fileExporter(isPresented: $isDocumentPickerPresented, document: ImageDocument(image: uiimage), contentType: .image, defaultFilename: "iconImage.png") { result in
+                    if case .success = result {
+                        print("Image saved successfully.")
+                    }
+                }
+            } footer: {Text("PassKeepr. Created by Drew Hanshaw")
+            }
+        }
+    }
+}
+
+#Preview {
+    Settings()
+}
