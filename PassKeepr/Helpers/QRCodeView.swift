efe623611f5b9aa923cfbdfa5fecfb87ac2177f2
Andrew Hanshaw <andrewhanshaw@2022-MacBook-Air.local>
2024-01-28 19:39:36 -0600

    Update QR Code generation

--- PassKeepr/Helpers/QRCodeView.swift
+++ PassKeepr/Helpers/QRCodeView.swift
@@ -8,12 +8,11 @@
 import SwiftUI
 
 struct QRCodeView: View {
-    var body: some View {
-        let QRCodeData = "Hello, Swift QRcode!"
+    var Data: String
+    var correctionLevel: QrCodeCorrectionLevel
 
-        // Generates a 2D QR code image
-        if let QRCodeImage = generateQRCode(from: QRCodeData, withViewWidth: UIScreen.main.bounds.width, with: "H") {
-            // Display the generated barcode image
+    var body: some View {
+        if let QRCodeImage = GenerateQRCode(string: Data, viewWidth: UIScreen.main.bounds.width, correctionLevel: correctionLevel) {
             Image(uiImage: QRCodeImage)
                 .resizable()
                 .scaledToFit()
@@ -21,33 +20,6 @@ struct QRCodeView: View {
     }
 }
 
-func generateQRCode(from string: String, withViewWidth viewWidth: CGFloat, with inputCorrectionLevel: String) -> UIImage? {
-    // Create a CIFilter named "CICode128BarcodeGenerator"
-    guard let filter = CIFilter(name: "CIQRCodeGenerator") else { return nil }
-
-    // Set the input message for the barcode
-    let data = string.data(using: String.Encoding.ascii)
-    filter.setValue(inputCorrectionLevel, forKey: "inputCorrectionLevel")
-    filter.setValue(data, forKey: "inputMessage")
-
-    // Get the output image from the filter
-    guard let outputImage = filter.outputImage else { return nil }
-
-    let xScale = viewWidth / outputImage.extent.size.width
-    let yScale = viewWidth / outputImage.extent.size.height
-
-    let scaledImage = outputImage.transformed(by: CGAffineTransform(scaleX: xScale, y: yScale))
-
-    // Convert the CIImage to a UIImage
-    let context = CIContext()
-    if let cgImage = context.createCGImage(scaledImage, from: scaledImage.extent) {
-        let qrCodeImage = UIImage(cgImage: cgImage, scale: UIScreen.main.scale, orientation: .up)
-        return qrCodeImage
-    }
-
-    return nil
-}
-
 #Preview {
-    QRCodeView()
+    QRCodeView(Data: "Hello, Swift QRcode!", correctionLevel: QrCodeCorrectionLevel.high)
 }
