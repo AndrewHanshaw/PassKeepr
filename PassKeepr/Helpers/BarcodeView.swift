ddb5bbd4c7ec0bcbd7b1e5140a415a07e253ca06
Andrew Hanshaw <andrewhanshaw@2022-MacBook-Air.local>
2024-01-28 19:42:57 -0600

    Refactor bacode generation

--- PassKeepr/Helpers/BarcodeView.swift
+++ PassKeepr/Helpers/BarcodeView.swift
@@ -12,40 +12,13 @@ struct BarcodeView: View {
         let barcodeData = "Hello, Swift Barcode!"
 
         // Generate a 1D barcode image
-        if let barcodeImage = generateBarcode(from: barcodeData,  withViewWidth: UIScreen.main.bounds.width) {
+        if let barcodeImage = GenerateBarcode(string: barcodeData,  viewWidth: UIScreen.main.bounds.width) {
             // Display the generated barcode image
             Image(uiImage: barcodeImage)
                 .resizable()
                 .scaledToFit()
         }
     }
-}
-
-func generateBarcode(from string: String, withViewWidth viewWidth: CGFloat) -> UIImage? {
-    // Create a CIFilter named "CICode128BarcodeGenerator"
-    guard let filter = CIFilter(name: "CICode128BarcodeGenerator") else { return nil }
-
-    // Set the input message for the barcode
-    let data = string.data(using: String.Encoding.ascii)
-    filter.setDefaults() // probably not needed?
-    filter.setValue(data, forKey: "inputMessage")
-
-    // Get the output image from the filter
-    guard let outputImage = filter.outputImage else { return nil }
-
-    let xScale = viewWidth / outputImage.extent.size.width
-    let yScale = viewWidth / outputImage.extent.size.height
-
-    let scaledImage = outputImage.transformed(by: CGAffineTransform(scaleX: xScale, y: yScale))
-
-    // Convert the CIImage to a UIImage
-    let context = CIContext()
-    if let cgImage = context.createCGImage(scaledImage, from: scaledImage.extent) {
-        let qrCodeImage = UIImage(cgImage: cgImage, scale: UIScreen.main.scale, orientation: .up)
-        return qrCodeImage
-    }
-
-    return nil
 }
 
 #Preview {
