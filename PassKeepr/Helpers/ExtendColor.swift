e15216febfb7097c0a6b8c355050c24be6328391
Andrew Hanshaw <andrewhanshaw@2022-MacBook-Air.local>
2024-01-28 19:32:18 -0600

    Move Color extensions to their own file

---
+++ PassKeepr/Helpers/ExtendColor.swift
@@ -0,0 +1,33 @@
+//
+//  ExtendColor.swift
+//  PassKeepr
+//
+//  Created by Andrew Hanshaw on 1/27/24.
+//
+
+import SwiftUI
+
+extension Color {
+    init(hex: UInt, alpha: Double = 1.0) {
+        self.init(
+            .sRGB,
+            red: Double((hex >> 16) & 0xFF) / 255.0,
+            green: Double((hex >> 8) & 0xFF) / 255.0,
+            blue: Double(hex & 0xFF) / 255.0,
+            opacity: alpha
+        )
+    }
+}
+
+extension Color {
+    func toHex() -> UInt {
+        @Environment(\.self) var environment
+        let resolvedColor = self.resolve(in: environment)
+
+        let red = UInt(resolvedColor.red * 255)
+        let green = UInt(resolvedColor.green * 255)
+        let blue = UInt(resolvedColor.blue * 255)
+
+        return (red << 16) + (green << 8) + blue
+    }
+}
