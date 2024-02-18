c0f1e108b8c27e17360650baaedaf1abb0dab346
Andrew Hanshaw <andrewhanshaw@2022-MacBook-Air.local>
2024-02-18 08:41:29 -0600

    Add generators and views for Code 39, 93, and UPCE

---
+++ PassKeepr/BarcodeGenerators/Code39View.swift
@@ -0,0 +1,113 @@
+//
+//  Code39View.swift
+//  PassKeepr
+//
+//  Created by Andrew Hanshaw on 2/5/24.
+//
+
+import SwiftUI
+
+struct Code39View: View {
+    @State var ratio: CGFloat
+    @State var value: String
+
+    var numberOfSegments: Int {
+        return (value.count * 13) + 27
+    }
+
+    var barcodeDataBuffer: Data {
+        stringToCode39BarcodeData(value) ?? Data()
+    }
+
+    var body: some View {
+        GeometryReader { geometry in
+            Rectangle()
+                .colorEffect(ShaderLibrary.OneDimensionalBarcodeFilter(.float(geometry.size.width), .data(barcodeDataBuffer), .data(Data([UInt8(numberOfSegments)]))))
+        }
+        .aspectRatio(ratio, contentMode: .fit)
+    }
+}
+
+#Preview {
+    Code39View(ratio: 1.0, value: "WIKIPEDIA")
+}
+
+import Foundation
+
+func stringToCode39BarcodeData(_ stringValue: String) -> Data? {
+    let characterTo13Bit: [Character: String] = [
+        "A": "1101010010110",
+        "B": "1011010010110",
+        "C": "1101101001010",
+        "D": "1010110010110",
+        "E": "1101011001010",
+        "F": "1011011001010",
+        "G": "1010100110110",
+        "H": "1101010011010",
+        "I": "1011010011010",
+        "J": "1010110011010",
+        "K": "1101010100110",
+        "L": "1011010100110",
+        "M": "1101101010010",
+        "N": "1010110100110",
+        "O": "1101011010010",
+        "P": "1011011010010",
+        "Q": "1010101100110",
+        "R": "1101010110010",
+        "S": "1011010110010",
+        "T": "1010110110010",
+        "U": "1100101010110",
+        "V": "1001101010110",
+        "W": "1100110101010",
+        "X": "1001011010110",
+        "Y": "1100101101010",
+        "Z": "1001101101010",
+        "0": "1010011011010",
+        "1": "1101001010110",
+        "2": "1011001010110",
+        "3": "1101100101010",
+        "4": "1010011010110",
+        "5": "1101001101010",
+        "6": "1011001101010",
+        "7": "1010010110110",
+        "8": "1101001011010",
+        "9": "1011001011010",
+        " ": "1001101011010",
+        "-": "1001010110110",
+        "$": "1001001001010",
+        "%": "1010010010010",
+        ".": "1100101011010",
+        "/": "1001001010010",
+        "+": "1001010010010",
+    ]
+
+    var binaryString = stringValue.enumerated().compactMap { index, character in
+        guard
+            let encodedCharacter = characterTo13Bit[character]
+        else {
+            return nil
+        }
+        return encodedCharacter
+    }.joined()
+
+    binaryString = "1001011011010" + binaryString + "100101101101" + "00000000"
+
+    var byteArray: [UInt8] = []
+    var index = binaryString.startIndex
+
+    while index < binaryString.endIndex {
+        let endIndex = binaryString.index(index, offsetBy: 8, limitedBy: binaryString.endIndex) ?? binaryString.endIndex
+        let byteString = binaryString[index..<endIndex]
+
+        if let byte = UInt8(byteString, radix: 2) {
+            byteArray.append(byte)
+        } else {
+            print("Invalid binary string.")
+            break
+        }
+
+        index = endIndex
+    }
+
+    return Data(byteArray)
+}
---
+++ PassKeepr/BarcodeGenerators/UPCEView.swift
@@ -0,0 +1,103 @@
+//
+//  UPCEView.swift
+//  PassKeepr
+//
+//  Created by Andrew Hanshaw on 2/4/24.
+//
+
+import SwiftUI
+
+struct UPCEView: View {
+    @State var ratio: CGFloat
+    @State var value: String
+
+    var barcodeDataBuffer: Data {
+        stringToUPCEBarcodeData(value, paritySequence: 0x15) ?? Data()
+    }
+
+    let numberOfSegments: UInt8 = 3 + 42 + 6
+
+    var body: some View {
+        GeometryReader { geometry in
+            Rectangle()
+                .colorEffect(ShaderLibrary.OneDimensionalBarcodeFilter(.float(geometry.size.width), .data(barcodeDataBuffer), .data(Data([numberOfSegments]))))
+        }
+        .aspectRatio(ratio, contentMode: .fit)
+    }
+}
+
+#Preview {
+    UPCEView(ratio: 1.0, value: "654321")
+}
+
+import Foundation
+
+func stringToUPCEBarcodeData(_ stringValue: String, paritySequence: UInt8) -> Data? {
+    // paritySequence is a 6 bit value, where 0 = even, 1 = odd
+
+    // Ensure the input is a 6-character string or pad with leading zeros
+    let paddedString = stringValue.padding(toLength: 6, withPad: "0", startingAt: 0)
+
+    let digitTo7BitEven: [Int: String] = [
+        0: "0100111", //1123
+        1: "0110011", //1222
+        2: "0011011", //2212
+        3: "0100001", //1141
+        4: "0011101", //2311
+        5: "0111001", //1321
+        6: "0000101", //4111
+        7: "0010001", //2131
+        8: "0001001", //3121
+        9: "0010111"  //2113
+    ]
+
+    let digitTo7BitOdd: [Int: String] = [
+        0: "0001101", //3211
+        1: "0011001", //2221
+        2: "0010011", //2122
+        3: "0111101", //1411
+        4: "0100011", //1132
+        5: "0110001", //1231
+        6: "0101111", //1114
+        7: "0111011", //1312
+        8: "0110111", //1213
+        9: "0001011"  //3112
+    ]
+
+    // Convert each digit to its 7-bit representation and concatenate
+    let binaryString = paddedString.enumerated().compactMap { index, digit in
+        guard
+            let digitValue = Int(String(digit)),
+            let sevenBitDigit = (((paritySequence >> index) & 0x1) != 0) ? digitTo7BitEven[digitValue] : digitTo7BitOdd[digitValue]
+        else {
+            return nil
+        }
+        return sevenBitDigit
+    }.joined()
+
+    // Append start and end patterns, append 0s so the resulting byte array
+    // is exactly the right length, otherwise the encoded string will not fill
+    // the bytes correctly. The default behavior when trying to pack bis to shift the value to the right
+    // which is not what we want here
+    // We ignore these bits when using the Metal filter.
+    let prefixedBinaryString: String = "101" + binaryString + "010101" + "00000000"
+
+    var byteArray: [UInt8] = []
+    var index = prefixedBinaryString.startIndex
+
+    while index < prefixedBinaryString.endIndex {
+        let endIndex = prefixedBinaryString.index(index, offsetBy: 8, limitedBy: prefixedBinaryString.endIndex) ?? prefixedBinaryString.endIndex
+        let byteString = prefixedBinaryString[index..<endIndex]
+
+        if let byte = UInt8(byteString, radix: 2) {
+            byteArray.append(byte)
+        } else {
+            print("Invalid binary string.")
+            break
+        }
+
+        index = endIndex
+    }
+
+    return Data(byteArray)
+}
