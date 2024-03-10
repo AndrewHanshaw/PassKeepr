2a24c3cb10b932c1cf0e02a843010225a0a41c62
Andrew Hanshaw <andrewhanshaw@2022-MacBook-Air.local>
2024-03-10 16:13:22 -0500

    Update barcodeInput

--- PassKeepr/Views/PassInputs/BarcodeInput.swift
+++ PassKeepr/Views/PassInputs/BarcodeInput.swift
@@ -11,16 +11,97 @@ struct BarcodeInput: View {
     @Binding var barcodeInput: String
 
     @State private var scannedCode = ""
+    @State private var scannedSymbology = ""
     @State private var isScannerPresented = false
     @State private var useScannedData = false
+    @State private var selectedBarcodeType: BarcodeType = .code39
+    @State private var showAlert: Bool = false
 
     var body: some View {
         Section {
+            Button(
+                action:{isScannerPresented.toggle()},
+                label:{
+                    HStack {
+                        Spacer()
+                        Text("Scan Existing Barcode")
+                            .foregroundColor(Color.accentColor)
+                            .disabled(false)
+                            .font(.system(size: 20))
+                        Spacer()
+                    }
+                    .padding([.top, .bottom], 10)
+                }
+            )
+            .sheet(isPresented: $isScannerPresented) {
+                ScannerView(scannedData: $scannedCode, scannedSymbology: $scannedSymbology, showScanner: $isScannerPresented)
+                    .edgesIgnoringSafeArea(.bottom)
+                    .presentationDragIndicator(.visible)
+            }
+        } footer: {
+            HStack() {
+                Spacer()
+                Text("Or:")
+                    .font(.system(size: 20))
+                Spacer()
+            }
+            .padding(.bottom, -999)
+            .padding(.top, 5)
+        }
+
+        Section {
+            HStack() {
+                Picker("Barcode Type", selection: $selectedBarcodeType) {
+                    ForEach(BarcodeType.allCases, id: \.self) { type in
+                        Text(String(describing: type))
+                    }
+                }
+                Spacer().frame(width: 25)
+                }
+                .overlay(
+                    HStack {
+                        Spacer()
+                        Button (
+                            action: {
+                                showAlert.toggle()
+                            },
+                            label: {
+                                Image(systemName: "info.circle")
+                                    .foregroundColor(Color(.secondaryLabel))
+                            })
+                        .buttonStyle(PlainButtonStyle())
+                        .alert(isPresented: $showAlert) {
+                            Alert(title: Text(String(describing: selectedBarcodeType)),
+                                  message: Text(BarcodeTypeHelpers.GetBarcodeTypeDescription(selectedBarcodeType)),
+                                  dismissButton: .default(Text("OK")))
+                        }
+                    }
+                )
+                .frame(maxWidth: .infinity, maxHeight: .infinity)
+                .layoutPriority(1)
+
             LabeledContent {
                 TextField("Barcode Number", text: $barcodeInput)
                     .keyboardType(.numberPad)
             } label : {
-                Text("Number")
+                Text("Data")
+            }
+        } footer: {
+            if(scannedSymbology != "" && scannedSymbology != "VNBarcodeSymbologyCode128") {
+                Text("Scanned code was not a valid barcode")
+            }
+        }
+
+        Section {
+            switch selectedBarcodeType {
+                case BarcodeType.code39:
+                    Code39View(ratio: 2, value: "WIKIPE")
+                case BarcodeType.code93:
+                    Code93View(ratio: 2, value: "TEST93")
+                case BarcodeType.upce:
+                    UPCEView(ratio: 2, value: "654321")
+                default:
+                    Text("default text")
             }
         }
     }
