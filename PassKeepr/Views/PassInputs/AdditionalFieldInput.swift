6c8883f932efa67f3d95632dcced386efdc41695
Andrew Hanshaw <andrewhanshaw@2022-MacBook-Air.local>
2024-03-10 15:37:12 -0500

    Add view for additional fields

---
+++ PassKeepr/Views/PassInputs/AdditionalFieldInput.swift
@@ -0,0 +1,41 @@
+//
+//  AdditionalFieldInput.swift
+//  PassKeepr
+//
+//  Created by Andrew Hanshaw on 3/10/24.
+//
+
+import SwiftUI
+
+struct AdditionalFieldInput: View {
+    @Binding var enableHeaderField: Bool
+    @Binding var labelInput: String
+    @Binding var textInput: String
+
+    var body: some View {
+        Section {
+            Toggle(isOn: $enableHeaderField) {
+                Text("Additional Header field")
+            }
+
+            if(enableHeaderField == true)
+            {
+                LabeledContent {
+                    TextField("Text", text: $textInput)
+                } label : {
+                    Text("Text")
+                }
+
+                LabeledContent {
+                    TextField("Optional", text: $labelInput)
+                } label : {
+                    Text("Label")
+                }
+            }
+        }
+    }
+}
+
+#Preview {
+    AdditionalFieldInput(enableHeaderField: .constant(false), labelInput:.constant(""), textInput: .constant(""))
+}
