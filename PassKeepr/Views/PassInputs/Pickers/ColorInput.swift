31156d5fbc4a183ebc0a707af3dda4e0d2a814b7
Drew Hanshaw <drew.hanshaw@cnhind.com>
2024-10-27 22:19:32 -0500

    Adjust spacing/padding

--- PassKeepr/Views/PassInputs/Pickers/ColorInput.swift
+++ PassKeepr/Views/PassInputs/Pickers/ColorInput.swift
@@ -6,11 +6,8 @@ struct ColorInput: View {
     var body: some View {
         VStack {
             ColorPicker("Background Color", selection: Color.binding(from: $pass.backgroundColor))
-                .padding(.top, 5)
             ColorPicker("Foreground Color", selection: Color.binding(from: $pass.foregroundColor))
-                .padding([.top, .bottom], 10)
             ColorPicker("Text Color", selection: Color.binding(from: $pass.textColor))
-                .padding(.bottom, 5)
         }
     }
 }
