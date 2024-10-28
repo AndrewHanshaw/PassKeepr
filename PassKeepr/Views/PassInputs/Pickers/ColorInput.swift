8c7763729b693d2c7c7f03639063e88bf748b9ac
Drew Hanshaw <drew.hanshaw@cnhind.com>
2024-10-27 22:20:23 -0500

    Add BackgroundImagePicker to ColorInput

--- PassKeepr/Views/PassInputs/Pickers/ColorInput.swift
+++ PassKeepr/Views/PassInputs/Pickers/ColorInput.swift
@@ -4,8 +4,23 @@ struct ColorInput: View {
     @Binding var pass: PassObject
 
     var body: some View {
-        VStack {
+        Section {
             ColorPicker("Background Color", selection: Color.binding(from: $pass.backgroundColor))
+        } footer: {
+            HStack {
+                Spacer()
+                Text("Or:")
+                    .font(.system(size: 20))
+                Spacer()
+            }
+            .padding(.bottom, -.infinity)
+            .padding(.top, 10)
+        }
+
+        Section {
+            BackgroundImagePicker(passObject: $pass)
+        }
+        Section {
             ColorPicker("Foreground Color", selection: Color.binding(from: $pass.foregroundColor))
             ColorPicker("Text Color", selection: Color.binding(from: $pass.textColor))
         }
