37405e7576837780bf5eec701780df51c2ca9614
Drew Hanshaw <drew.hanshaw@cnhind.com>
2024-11-11 16:48:00 -0600

    Fix UInt toRGBString extension

--- PassKeepr/Helpers/ExtendUInt.swift
+++ PassKeepr/Helpers/ExtendUInt.swift
@@ -1,9 +1,9 @@
 
 extension UInt {
-    func toRGBString(hex: UInt) -> String {
-        let red = (hex >> 16) & 0xFF
-        let green = (hex >> 8) & 0xFF
-        let blue = hex & 0xFF
+    func toRGBString() -> String {
+        let red = (self >> 16) & 0xFF
+        let green = (self >> 8) & 0xFF
+        let blue = self & 0xFF
         return "rgb(\(red), \(green), \(blue))"
     }
 }
