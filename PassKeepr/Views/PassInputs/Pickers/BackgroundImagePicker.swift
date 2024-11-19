d7cf9f436beb9d55b270ad30de415e116da5f691
Drew Hanshaw <drew.hanshaw@cnhind.com>
2024-11-18 21:15:28 -0600

    Fix image pickers to clip touch target for photos picker

--- PassKeepr/Views/PassInputs/Pickers/BackgroundImagePicker.swift
+++ PassKeepr/Views/PassInputs/Pickers/BackgroundImagePicker.swift
@@ -11,52 +11,49 @@ struct BackgroundImagePicker: View {
 
     var body: some View {
         Section {
-            VStack {
-                if UIImage(data: passObject.backgroundImage) != nil {
-                    HStack {
-                        Spacer()
-
-                        Image(uiImage: UIImage(data: passObject.backgroundImage)!)
-                            .resizable()
-                            .scaledToFit()
-                            .blur(radius: 6)
-                            .mask(
-                                Rectangle()
-                                    .cornerRadius(10.0)
-                            )
-                            .padding([.top, .bottom], 16)
-                        Spacer()
-                    }
-                    Divider()
-                }
-
+            if UIImage(data: passObject.backgroundImage) != nil {
                 HStack {
                     Spacer()
-                    PhotosPicker("Choose Background Image", selection: $photoItem, matching: .any(of: [.images, .not(.videos)]))
+
+                    Image(uiImage: UIImage(data: passObject.backgroundImage)!)
+                        .resizable()
+                        .scaledToFit()
+                        .blur(radius: 6)
+                        .mask(
+                            Rectangle()
+                                .cornerRadius(10.0)
+                        )
+                        .padding([.top, .bottom], 16)
                     Spacer()
-                    Button(
-                        action: {
-                            showAlert.toggle()
-                        },
-                        label: {
-                            Image(systemName: "info.circle")
-                                .foregroundColor(Color(.secondaryLabel))
-                        }
-                    )
-                    .buttonStyle(PlainButtonStyle())
-                    .alert(isPresented: $showAlert) {
-                        Alert(title: Text(alertTitleText),
-                              message: Text(alertDescriptionText),
-                              dismissButton: .default(Text("OK")))
-                    }
                 }
-                .onChange(of: photoItem) {
-                    Task {
-                        if let loaded = try? await photoItem?.loadTransferable(type: Data.self) {
-                            passObject.backgroundImage = UIImage(data: loaded)!.resize(targetSize: CGSize(width: 224, height: 284))!.pngData()!
-                        } else {
-                            print("Failed")
-                        }
+            }
+
+            HStack {
+                Spacer()
+                PhotosPicker("Choose Background Image", selection: $photoItem, matching: .any(of: [.images, .not(.videos)]))
+                Spacer()
+                Button(
+                    action: {
+                        showAlert.toggle()
+                    },
+                    label: {
+                        Image(systemName: "info.circle")
+                            .foregroundColor(Color(.secondaryLabel))
+                    }
+                )
+                .buttonStyle(PlainButtonStyle())
+                .alert(isPresented: $showAlert) {
+                    Alert(title: Text(alertTitleText),
+                          message: Text(alertDescriptionText),
+                          dismissButton: .default(Text("OK")))
+                }
+            }
+            .onChange(of: photoItem) {
+                Task {
+                    if let loaded = try? await photoItem?.loadTransferable(type: Data.self) {
+                        passObject.backgroundImage = UIImage(data: loaded)!.resize(targetSize: CGSize(width: 224, height: 284))!.pngData()!
+                    } else {
+                        print("Failed")
                     }
                 }
             }
