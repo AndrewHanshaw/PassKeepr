36d847c118aae0b76f3f752670532a60b61483d4
Drew Hanshaw <drew.hanshaw@cnhind.com>
2025-01-15 20:29:42 -0600

    Header text field

--- PassKeepr/Views/PassCard/HeaderTextField.swift
+++ PassKeepr/Views/PassCard/HeaderTextField.swift
@@ -11,45 +11,43 @@ struct HeaderTextField: View {
 
     var body: some View {
         VStack {
-            ZStack {
-                if textLabel != "" || text != "" {
-                    HStack(alignment: .top) {
-                        Spacer()
-                        VStack {
-                            HStack {
-                                Spacer()
-                                Text(textLabel)
-                                    .lineLimit(1)
-                                    .frame(alignment: .top)
-                                    .foregroundColor(labelColor)
-                                    .disableAutocorrection(true)
-                                    .textCase(.uppercase)
-                                    .font(.system(size: 8))
-                                    .fontWeight(.semibold)
-                                    .padding(0)
-                                    .padding(.leading, -10)
-                                    .keyboardType(.asciiCapable)
-//                                    .minimumScaleFactor(0.34) // TODO: Is this needed?
-                            }
-
-                            HStack {
-                                Spacer()
-                                Text(text)
-                                    .lineLimit(1)
-                                    .frame(alignment: .top)
-                                    .foregroundColor(textColor)
-                                    .disableAutocorrection(true)
-                                    .font(.system(size: 10))
-                                    .padding(0)
-                                    .padding(.leading, -10)
-                                    .minimumScaleFactor(0.34)
-                            }
+            if textLabel != "" || text != "" {
+                HStack(alignment: .top) {
+                    Spacer()
+                    VStack {
+                        HStack {
                             Spacer()
+                            Text(textLabel)
+                                .lineLimit(1)
+                                .frame(alignment: .top)
+                                .foregroundColor(labelColor)
+                                .disableAutocorrection(true)
+                                .textCase(.uppercase)
+                                .font(.system(size: 8))
+                                .fontWeight(.semibold)
+                                .padding(0)
+                                .padding(.leading, -10)
+                                .keyboardType(.asciiCapable)
+//                                    .minimumScaleFactor(0.34) // TODO: Is this needed?
                         }
+
+                        HStack {
+                            Spacer()
+                            Text(text)
+                                .lineLimit(1)
+                                .frame(alignment: .top)
+                                .foregroundColor(textColor)
+                                .disableAutocorrection(true)
+                                .font(.system(size: 10))
+                                .padding(0)
+                                .padding(.leading, -10)
+                                .minimumScaleFactor(0.34)
+                        }
+                        Spacer()
                     }
                 }
+                .frame(maxWidth: .infinity)
             }
-            .frame(maxWidth: .infinity)
             Spacer()
         }
     }
