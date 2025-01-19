7cce67ba588e91aa062695f2aa1559a06bf35eec
Drew Hanshaw <drew.hanshaw@cnhind.com>
2025-01-19 16:22:28 -0600

    WIP

---
+++ PassKeepr/Views/PassInputs/Pickers/CustomizeBackgroundImage.swift
@@ -0,0 +1,57 @@
+import _PhotosUI_SwiftUI
+import CoreImage
+import SwiftUI
+
+struct CustomizeBackgroundImage: View {
+    @Binding var passObject: PassObject
+
+    @State private var tempBackground: UIImage
+
+    @State private var photoItem: PhotosPickerItem?
+
+    @Environment(\.presentationMode) var presentationMode: Binding<PresentationMode>
+
+    init(passObject: Binding<PassObject>) {
+        _passObject = passObject
+        _tempBackground = State(initialValue: UIImage(data: passObject.wrappedValue.backgroundImage)!)
+    }
+
+    var body: some View {
+        Image(uiImage: tempBackground)
+            .resizable()
+            .scaledToFit()
+            .padding(20)
+
+        List {
+            PhotosPicker("Change image", selection: $photoItem, matching: .any(of: [.images, .not(.videos)]))
+                .frame(maxWidth: .infinity, alignment: .center)
+                .onChange(of: photoItem) {
+                    Task {
+                        if let loaded = try? await photoItem?.loadTransferable(type: Data.self) {
+                            tempBackground = UIImage(data: loaded)!
+                        } else {
+                            print("Failed")
+                        }
+                    }
+                }
+
+            Section {
+                Button(
+                    action: {
+                        passObject.backgroundImage = tempBackground.pngData()!
+                        presentationMode.wrappedValue.dismiss()
+                    }) {
+                        Text("Save")
+                            .fontWeight(.bold)
+                            .foregroundColor(Color.white)
+                            .frame(maxWidth: .infinity, alignment: .center)
+                    }
+            }
+            .listRowBackground(Color.accentColor)
+        }
+    }
+}
+
+#Preview {
+    CustomizeLogoImage(passObject: .constant(MockModelData().passObjects[0]))
+}
