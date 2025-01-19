e0e645a634ce2249b98a2b966186d44e43ce69e0
Drew Hanshaw <drew.hanshaw@cnhind.com>
2025-01-19 16:00:21 -0600

    WIP don't toggle colorInput option visibility?

--- PassKeepr/Views/PassInputs/Pickers/ColorInput.swift
+++ PassKeepr/Views/PassInputs/Pickers/ColorInput.swift
@@ -6,27 +6,6 @@ struct ColorInput: View {
     var body: some View {
         Section {
             ColorPicker("Background Color", selection: Color.binding(from: $pass.backgroundColor))
-        }
-
-        if getIsBackgroundImageSupported(passObject: pass) {
-            Section {
-                HStack {
-                    Spacer()
-                    Text("Or:")
-                        .font(.system(size: 20))
-                        .foregroundColor(.secondary)
-                    Spacer()
-                }
-            }
-            .listSectionSpacing(0)
-            .listRowBackground(Color.clear)
-
-            Section {
-                BackgroundImagePicker(passObject: $pass)
-            }
-        }
-
-        Section {
             ColorPicker("Foreground Color", selection: Color.binding(from: $pass.foregroundColor))
             ColorPicker("Label Color", selection: Color.binding(from: $pass.labelColor))
         }
