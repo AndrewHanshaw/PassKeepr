b8925f7934f824f97a1546bef74a1f887f6568ed
Andrew Hanshaw <drew@andrewhanshaw.com>
2025-10-26 15:16:55 -0500

    Update CustomizeQrCode to not use List

--- PassKeepr/Views/PassInputs/Pickers/CustomizeQrCode.swift
+++ PassKeepr/Views/PassInputs/Pickers/CustomizeQrCode.swift
@@ -4,6 +4,8 @@ import SwiftUI
 import Vision
 
 struct CustomizeQrCode: View {
+    @Environment(\.colorScheme) var colorScheme
+
     @Binding var passObject: PassObject
     @State private var photoItem: PhotosPickerItem?
     @State private var imageToScanForQrCodes: UIImage?
@@ -33,34 +35,66 @@ struct CustomizeQrCode: View {
     }
 
     var body: some View {
-        List {
-            Section {
-                if tempQrCodeData != "" {
-                    QRCodeView(data: tempQrCodeData, correctionLevel: tempQrCodeCorrectionLevel, encoding: tempQrCodeEncoding).aspectRatio(1, contentMode: .fit)
-                        .onChange(of: tempQrCodeEncoding) {
-                            print("qr code encoding \(tempQrCodeEncoding)")
+        NavigationView {
+            ScrollView {
+                VStack(spacing: 20) {
+                    Group {
+                        if tempQrCodeData != "" {
+                            QRCodeView(data: tempQrCodeData, correctionLevel: tempQrCodeCorrectionLevel, encoding: tempQrCodeEncoding).aspectRatio(1, contentMode: .fit)
+                                .onChange(of: tempQrCodeEncoding) {
+                                    print("qr code encoding \(tempQrCodeEncoding)")
+                                }
+                        } else {
+                            ZStack {
+                                RoundedRectangle(cornerRadius: 5)
+                                    .stroke(style: StrokeStyle(lineWidth: 2, dash: [5, 3]))
+                                    .aspectRatio(1, contentMode: .fit)
+                                    .foregroundColor(Color.gray)
+                                    .opacity(0.5)
+                                Text("Enter QR Code Data")
+                                    .foregroundColor(Color.gray)
+                                    .opacity(0.7)
+                            }
+                            .frame(maxWidth: .infinity, alignment: .center)
                         }
-                } else {
-                    Text("Enter QR Code Data")
-                        .frame(maxWidth: .infinity, maxHeight: .infinity)
-                        .padding(20)
-                        .aspectRatio(1, contentMode: .fit)
-                }
-            }
-            .onChange(of: scannedCode) {
-                tempQrCodeData = scannedCode
-            }
+                    }
+                    .clipShape(.rect(cornerRadius: 5))
 
-            Section {
-                Button(
-                    action: { isScannerPresented.toggle() },
-                    label: {
+                    Button(
+                        action: { isScannerPresented.toggle() },
+                        label: {
+                            HStack {
+                                Image(systemName: "qrcode.viewfinder")
+                                    .font(.system(size: 40))
+                                    .foregroundColor(Color(.label))
+
+                                Text("Scan Existing QR Code")
+                                    .font(.system(size: 20))
+                                    .foregroundColor(Color(.label))
+                                    .disabled(false)
+                            }
+                            .padding([.top, .bottom], 10)
+                            .frame(maxWidth: .infinity, alignment: .center)
+                        }
+                    )
+                    .listSectionBackgroundModifier()
+                    .sheet(isPresented: $isScannerPresented) {
+                        ScannerView(scannedData: $scannedCode, scannedBarcodeType: $scannedBarcodeType, showScanner: $isScannerPresented)
+                            .edgesIgnoringSafeArea(.bottom)
+                    }
+
+                    Text("Or:")
+                        .frame(maxWidth: .infinity, alignment: .center)
+                        .font(.system(size: 20))
+                        .foregroundColor(.secondary)
+
+                    PhotosPicker(selection: $photoItem, matching: .any(of: [.images, .not(.videos)])) {
                         HStack {
-                            Image(systemName: "qrcode.viewfinder")
+                            Image(systemName: "photo")
                                 .font(.system(size: 40))
                                 .foregroundColor(Color(.label))
 
-                            Text("Scan Existing QR Code")
+                            Text("Get QR Code from Image")
                                 .font(.system(size: 20))
                                 .foregroundColor(Color(.label))
                                 .disabled(false)
@@ -68,138 +102,128 @@ struct CustomizeQrCode: View {
                         .padding([.top, .bottom], 10)
                         .frame(maxWidth: .infinity, alignment: .center)
                     }
-                )
-                .sheet(isPresented: $isScannerPresented) {
-                    ScannerView(scannedData: $scannedCode, scannedBarcodeType: $scannedBarcodeType, showScanner: $isScannerPresented)
-                        .edgesIgnoringSafeArea(.bottom)
-                        .presentationDragIndicator(.visible)
-                }
-            }
-            .listSectionSeparator(.hidden)
+                    .listSectionBackgroundModifier()
+                    .onChange(of: photoItem) {
+                        Task {
+                            if let loaded = try? await photoItem?.loadTransferable(type: Data.self) {
+                                imageToScanForQrCodes = UIImage(data: loaded)!
+                            } else {
+                                print("Failed")
+                            }
+                        }
+                    }
+                    .onChange(of: imageToScanForQrCodes) {
+                        Task {
+                            if let imageToScanForQrCodes {
+                                if let imageBarcode = GetBarcodeFromImage(image: imageToScanForQrCodes) {
+                                    // only allow scanning of qr codes
+                                    if BarcodeType.qr != imageBarcode.barcodeType {
+                                        showInvalidQrCodeAlert.toggle()
+                                    } else {
+                                        tempQrCodeData = imageBarcode.payload
+                                    }
+                                } else {
+                                    showInvalidQrCodeAlert.toggle()
+                                }
+                            }
+                        }
+                    }
 
-            Section {
-                Text("Or:")
-                    .frame(maxWidth: .infinity, alignment: .center)
-                    .font(.system(size: 20))
-                    .foregroundColor(.secondary)
-            }
-            .listSectionSpacing(0)
-            .listRowBackground(Color.clear)
+                    Text("Or:")
+                        .frame(maxWidth: .infinity, alignment: .center)
+                        .font(.system(size: 20))
+                        .foregroundColor(.secondary)
 
-            Section {
-                PhotosPicker(selection: $photoItem, matching: .any(of: [.images, .not(.videos)])) {
+                    TextEditor(text: $tempQrCodeData)
+                        .listSectionTextEditorModifier(placeholderText: "QR Code Data", isEnteredTextEmpty: tempQrCodeData.isEmpty)
+
                     HStack {
-                        Image(systemName: "photo")
-                            .font(.system(size: 40))
-                            .foregroundColor(Color(.label))
-
-                        Text("Get QR Code from Image")
-                            .font(.system(size: 20))
-                            .foregroundColor(Color(.label))
-                            .disabled(false)
+                        Text("Correction Level")
+                        Spacer()
+                        Picker("Correction Level", selection: $tempQrCodeCorrectionLevel) {
+                            ForEach(QrCodeCorrectionLevel.allCases, id: \.self) { level in
+                                Text(String(describing: level))
+                            }
+                        }
+                        .accentColor(.secondary)
                     }
                     .padding([.top, .bottom], 10)
-                    .frame(maxWidth: .infinity, alignment: .center)
-                }
-            }
-            .onChange(of: photoItem) {
-                Task {
-                    if let loaded = try? await photoItem?.loadTransferable(type: Data.self) {
-                        imageToScanForQrCodes = UIImage(data: loaded)!
-                    } else {
-                        print("Failed")
+                    .padding(.trailing, 4)
+                    .padding(.leading, 12)
+                    .listSectionBackgroundModifier()
+                    .onChange(of: tempQrCodeCorrectionLevel) {
+                        scannedBarcodeType = nil
                     }
-                }
-            }
-            .onChange(of: imageToScanForQrCodes) {
-                Task {
-                    if let imageToScanForQrCodes {
-                        if let imageBarcode = GetBarcodeFromImage(image: imageToScanForQrCodes) {
-                            // only allow scanning of qr codes
-                            if BarcodeType.qr != imageBarcode.barcodeType {
-                                showInvalidQrCodeAlert.toggle()
-                            } else {
-                                tempQrCodeData = imageBarcode.payload
+
+                    HStack {
+                        Text("Encoding")
+                        Spacer()
+                        Picker("Encoding", selection: $tempQrCodeEncoding) {
+                            ForEach(QrCodeEncoding.allCases, id: \.self) { encoding in
+                                Text(String(describing: encoding))
                             }
-                        } else {
-                            showInvalidQrCodeAlert.toggle()
                         }
+                        .accentColor(.secondary)
                     }
+                    .padding([.top, .bottom], 10)
+                    .padding(.trailing, 4)
+                    .padding(.leading, 12)
+                    .listSectionBackgroundModifier()
+                    .onChange(of: tempQrCodeEncoding) {
+                        scannedBarcodeType = nil
+                    }
+
+                    LabeledContent {
+                        TextField("Alt text", text: $tempAltText)
+                            .disableAutocorrection(true)
+                    } label: {
+                        Text("Text")
+                    }
+                    .padding(14)
+                    .listSectionBackgroundModifier()
+
+                    Spacer()
                 }
-            }
+                .padding(.top, 60)
+                .padding()
+                .toolbar {
+                    ToolbarItem(placement: .principal) {
+                        Text("Customize QR Code")
+                            .font(.system(size: 16, weight: .bold, design: .rounded))
+                    }
 
-            Section {
-                Text("Or:")
-                    .frame(maxWidth: .infinity, alignment: .center)
-                    .font(.system(size: 20))
-                    .foregroundColor(.secondary)
-            }
-            .listSectionSpacing(0)
-            .listRowBackground(Color.clear)
-
-            Section {
-                TextEditor(text: $tempQrCodeData)
-                    .overlay(alignment: .topLeading) {
-                        if tempQrCodeData.isEmpty {
-                            Text("Data")
-                                .foregroundColor(.gray.opacity(0.7))
-                                .padding(.horizontal, 5)
-                                .padding(.vertical, 8)
-                                .allowsHitTesting(false)
+                    ToolbarItem(placement: .confirmationAction) {
+                        Button("Save", systemImage: "checkmark") {
+                            passObject.barcodeString = tempQrCodeData
+                            passObject.qrCodeCorrectionLevel = tempQrCodeCorrectionLevel
+                            passObject.altText = tempAltText
+                            presentationMode.wrappedValue.dismiss()
                         }
+                        .toolbarConfirmButtonModifier()
                     }
 
-                Picker("Correction Level", selection: $tempQrCodeCorrectionLevel) {
-                    ForEach(QrCodeCorrectionLevel.allCases, id: \.self) { level in
-                        Text(String(describing: level))
+                    ToolbarItem(placement: .cancellationAction) {
+                        Button("Cancel", systemImage: "xmark") {
+                            presentationMode.wrappedValue.dismiss()
+                        }
+                        .toolbarCancelButtonModifier()
                     }
-                }
-                .onChange(of: tempQrCodeCorrectionLevel) {
-                    scannedBarcodeType = nil
-                }
-
-                Picker("Encoding", selection: $tempQrCodeEncoding) {
-                    ForEach(QrCodeEncoding.allCases, id: \.self) { encoding in
-                        Text(String(describing: encoding))
-                    }
-                }
-                .onChange(of: tempQrCodeEncoding) {
-                    scannedBarcodeType = nil
                 }
             }
+            .keyboardAware()
+            .scrollDismissesKeyboard(.immediately)
+            .ignoresSafeArea(edges: .all) // otherwise it gets all wiggy when you flick scroll to the top or bottom
+            .highPriorityGesture(DragGesture()) // Fix for an iOS 18 bug. Otherwise if you drag with your finger on a button it will click that button. (see https://www.reddit.com/r/SwiftUI/comments/1hf4wwq/sheet_button_triggering_while_scrolling/)
+            .background(colorScheme == .light ? Color(UIColor.secondarySystemBackground) : Color(UIColor.systemBackground))
             .alert(isPresented: $showInvalidQrCodeAlert) {
                 Alert(title: Text("No valid QR code Detected"),
                       message: Text("Please select an image containing a valid QR code"),
                       dismissButton: .default(Text("OK")))
             }
-
-            Section {
-                LabeledContent {
-                    TextField("Alt text", text: $tempAltText)
-                        .disableAutocorrection(true)
-                } label: {
-                    Text("Text")
-                }
+            .onChange(of: scannedCode) {
+                tempQrCodeData = scannedCode
             }
-
-            Section {
-                Button(
-                    action: {
-                        passObject.barcodeString = tempQrCodeData
-                        passObject.qrCodeCorrectionLevel = tempQrCodeCorrectionLevel
-                        passObject.altText = tempAltText
-                        presentationMode.wrappedValue.dismiss()
-                    }) {
-                        Text("Save")
-                            .fontWeight(.bold)
-                            .foregroundColor(Color.white)
-                            .frame(maxWidth: .infinity, alignment: .center)
-                    }
-            }
-            .listRowBackground(Color.accentColor)
         }
-        .keyboardAware()
-        .scrollDismissesKeyboard(.immediately)
     }
 }
 
