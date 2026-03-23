a8cfe92c7dcbcef5ef055565931210eb1f9e687c
Andrew Hanshaw <drew@andrewhanshaw.com>
2026-03-22 21:52:01 -0500

    Refactor PassCardTopSection

--- PassKeepr/Views/PassCard/PassCardTopSection.swift
+++ PassKeepr/Views/PassCard/PassCardTopSection.swift
@@ -5,26 +5,24 @@ struct PassCardTopSection: View {
     @State private var viewSize: CGSize = CGSizeZero
 
     var body: some View {
-        GeometryReader { _ in
-            HStack {
-                if passObject.logoImage != Data() {
-                    VStack {
-                        Image(uiImage: UIImage(data: passObject.logoImage)!)
-                            .resizable()
-                            .scaledToFit()
-                            .frame(maxHeight: 30)
-                            .padding([.top, .leading], 8)
-                        Spacer()
-                    }
-                }
-                Spacer()
-                if passObject.headerFieldOneLabel != "" && passObject.headerFieldOneText != "" {
-                    HeaderTextField(textLabel: passObject.headerFieldOneLabel, text: passObject.headerFieldOneText, textColor: Color(hex: passObject.foregroundColor), labelColor: Color(hex: passObject.labelColor))
-                        .padding(.top, 5)
+        HStack {
+            if passObject.logoImage != Data() {
+                VStack {
+                    Image(uiImage: UIImage(data: passObject.logoImage)!)
+                        .resizable()
+                        .scaledToFit()
+                        .frame(maxHeight: 30)
+                        .padding([.top, .leading], 8)
+                    Spacer()
                 }
             }
-            .padding(.trailing, 10)
+            Spacer()
+            if passObject.headerFieldOneLabel != "" && passObject.headerFieldOneText != "" {
+                HeaderTextField(textLabel: passObject.headerFieldOneLabel, text: passObject.headerFieldOneText, textColor: Color(hex: passObject.foregroundColor), labelColor: Color(hex: passObject.labelColor))
+                    .padding(.top, 5)
+            }
         }
+        .padding(.trailing, 10)
     }
 }
 
