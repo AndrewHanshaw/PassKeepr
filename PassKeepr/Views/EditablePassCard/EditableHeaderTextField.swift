6cadb64479a7cec7c112a69fcd0a0156e4a37f80
Andrew Hanshaw <drew@andrewhanshaw.com>
2026-03-22 21:49:58 -0500

    Refactor EditableHeaderTextField

--- PassKeepr/Views/EditablePassCard/EditableHeaderTextField.swift
+++ PassKeepr/Views/EditablePassCard/EditableHeaderTextField.swift
@@ -13,42 +13,27 @@ struct EditableHeaderTextField: View {
     @State private var isCustomizeTextPresented = false
 
     var body: some View {
-        VStack {
-            ZStack {
-                if textLabel != "" || text != "" {
-                    HStack(alignment: .top) {
-                        Spacer()
-                        VStack {
-                            HStack {
-                                Spacer()
-                                Text(textLabel)
-                                    .lineLimit(1)
-                                    .frame(alignment: .top)
-                                    .foregroundColor(labelColor)
-                                    .disableAutocorrection(true)
-                                    .textCase(.uppercase)
-                                    .font(.system(size: 11))
-                                    .fontWeight(.semibold)
-                                    .padding(0)
-                                    .padding(.leading, -20)
-                            }
+        VStack(alignment: .trailing, spacing: 0) {
+            if textLabel != "" || text != "" {
+                Text(textLabel)
+                    .lineLimit(1)
+                    .frame(alignment: .topTrailing)
+                    .foregroundColor(labelColor)
+                    .disableAutocorrection(true)
+                    .textCase(.uppercase)
+                    .font(.system(size: 11))
+                    .fontWeight(.semibold)
+                    .padding(0)
 
-                            HStack {
-                                Spacer()
-                                Text(text)
-                                    .lineLimit(1)
-                                    .frame(alignment: .top)
-                                    .foregroundColor(textColor)
-                                    .disableAutocorrection(true)
-                                    .font(.system(size: 22))
-                                    .padding(0)
-                                    // .padding(.leading, -20)
-                                    .minimumScaleFactor(0.34)
-                            }
-                            Spacer()
-                        }
-                    }
-                } else {
+                Text(text)
+                    .lineLimit(1)
+                    .frame(maxHeight: .infinity, alignment: .topTrailing)
+                    .foregroundColor(textColor)
+                    .disableAutocorrection(true)
+                    .padding(0)
+                    .minimumScaleFactor(0.34) // no font size specified. Just let it be as large as possible and shrink it if needed
+            } else {
+                ZStack {
                     RoundedRectangle(cornerRadius: 5)
                         .stroke(style: StrokeStyle(lineWidth: 2, dash: [5, 3]))
                         .foregroundColor(backgroundBrightness.overwriteForegroundColor)
@@ -59,27 +44,26 @@ struct EditableHeaderTextField: View {
                         .foregroundColor(backgroundBrightness.overwriteForegroundColor)
                         .opacity(backgroundBrightness.overwriteOpacity)
                 }
-
-                Button(action: {
-                    isCustomizeTextPresented.toggle()
-                }) {
-                    Image(systemName: "pencil.circle.fill")
-                        .symbolRenderingMode(.palette)
-                        .foregroundStyle(.green, .white)
-                        .frame(maxWidth: .infinity, alignment: .bottomTrailing)
-                        .font(.system(size: 18))
-                        .offset(x: 9, y: 18)
-                        .shadow(radius: 3, x: 0, y: 0)
-                }
-                .buttonStyle(PlainButtonStyle())
-                .disabled(disableButton)
             }
-            .frame(maxWidth: .infinity)
-            .popover(isPresented: $isCustomizeTextPresented, arrowEdge: .top) {
-                CustomizePassTextField(textLabel: $textLabel, text: $text)
-                    .presentationCompactAdaptation((.popover))
+        }
+        .overlay {
+            Button(action: {
+                isCustomizeTextPresented.toggle()
+            }) {
+                Image(systemName: "pencil.circle.fill")
+                    .symbolRenderingMode(.palette)
+                    .foregroundStyle(.green, .white)
+                    .frame(maxWidth: .infinity, alignment: .bottomTrailing)
+                    .font(.system(size: 18))
+                    .offset(x: 9, y: 18)
+                    .shadow(radius: 3, x: 0, y: 0)
             }
-            Spacer()
+            .buttonStyle(PlainButtonStyle())
+            .disabled(disableButton)
+        }
+        .popover(isPresented: $isCustomizeTextPresented, arrowEdge: .top) {
+            CustomizePassTextField(textLabel: $textLabel, text: $text)
+                .presentationCompactAdaptation((.popover))
         }
     }
 }
