f90d0588e9b094d5103e636f51e660d32d8408b3
Andrew Hanshaw <drew@andrewhanshaw.com>
2026-03-25 21:14:48 -0500

    Disable strip image and add message when barcode type is QR

--- PassKeepr/Views/PassInputs/Pickers/StripImageSelection.swift
+++ PassKeepr/Views/PassInputs/Pickers/StripImageSelection.swift
@@ -8,10 +8,29 @@ struct StripImageSelection: View {
     var disableControl: Bool
 
     var body: some View {
-        Toggle("Strip Image", isOn: $passObject.isCustomStripImageOn)
-            .padding(14)
-            .listSectionBackgroundModifier()
-            .disabled(disableControl)
+        VStack(alignment: .leading) {
+            Toggle("Strip Image", isOn: $passObject.isCustomStripImageOn)
+                .foregroundColor(passObject.barcodeType == .qr ? .secondary : .primary)
+                .padding(14)
+                .listSectionBackgroundModifier()
+                .disabled(disableControl || passObject.barcodeType == .qr)
+                .onChange(of: passObject.barcodeType) { newType in
+                    if newType == .qr {
+                        passObject.isCustomStripImageOn = false
+                    }
+                }
+                .onAppear {
+                    if passObject.barcodeType == .qr {
+                        passObject.isCustomStripImageOn = false
+                    }
+                }
+            if passObject.barcodeType == .qr {
+                Text("Strip images are not compatible with QR codes")
+                    .font(.footnote)
+                    .foregroundColor(.secondary)
+                    .padding([.leading, .bottom], 14)
+            }
+        }
     }
 }
 
