7b460b8fec58400448d318020a676d406ba25fea
Andrew Hanshaw <drew@andrewhanshaw.com>
2026-04-14 21:40:48 -0500

    Remove ability to 'offset' strip images

--- PassKeepr/Views/PassInputs/Pickers/CustomizeStripImage.swift
+++ PassKeepr/Views/PassInputs/Pickers/CustomizeStripImage.swift
@@ -1,15 +1,10 @@
 import _PhotosUI_SwiftUI
-import CoreImage
 import SwiftUI
 
 struct CustomizeStripImage: View {
     @Environment(\.colorScheme) var colorScheme
 
     @Binding var passObject: PassObject
-    @State private var cropOffset: CGFloat = 0.0
-    @State private var size: CGSize = CGSizeZero
-    @State private var offsetBound: CGFloat = 0
-
     @State private var tempStrip: UIImage?
 
     @State private var photoItem: PhotosPickerItem?
@@ -26,37 +21,30 @@ struct CustomizeStripImage: View {
     var body: some View {
         NavigationView {
             VStack(spacing: 20) {
-                if let strip = tempStrip {
-                    OffsetCroppedStripImage(cropOffset: cropOffset, strip: strip)
-                        .readSize(into: $size)
-                        .padding([.top, .bottom], 20)
-                        .onAppear {
-                            offsetBound = (((size.width / strip.size.width) * strip.size.height) - size.height) / 2 // Scale the image so it's the same width as the rectangle, then subtract the height difference to get the quantity of vertical pts that are cropped out. Then diviide that by 2 so the offset can go +/- that value
-                            print("offsetBound \(offsetBound)")
-                        }
-                        .onChange(of: strip) {
-                            Task {
-                                offsetBound = (((size.width / strip.size.width) * strip.size.height) - size.height) / 2
-                                cropOffset = 0
-                                print("cropOffset \(cropOffset)")
-                            }
-                        }
-                } else {
-                    ZStack {
-                        RoundedRectangle(cornerRadius: 5)
-                            .stroke(style: StrokeStyle(lineWidth: 2, dash: [5, 3]))
-                            .aspectRatio(PassKitConstants.StripImage.aspectRatio, contentMode: .fit)
-                            .foregroundColor(Color.gray)
-                            .opacity(0.5)
-                        Text("Add a Strip Image")
+                Group {
+                    if let strip = tempStrip {
+                        Image(uiImage: strip)
+                            .resizable()
                             .scaledToFit()
-                            .textCase(nil)
-                            .foregroundColor(Color.gray)
-                            .opacity(0.7)
+                            .frame(maxWidth: .infinity, maxHeight: PassKitConstants.StripImage.height)
+                            .clipped()
+                    } else {
+                        ZStack {
+                            RoundedRectangle(cornerRadius: 5)
+                                .stroke(style: StrokeStyle(lineWidth: 2, dash: [5, 3]))
+                                .aspectRatio(PassKitConstants.StripImage.aspectRatio, contentMode: .fit)
+                                .foregroundColor(Color.gray)
+                                .opacity(0.5)
+                            Text("Add a Strip Image")
+                                .scaledToFit()
+                                .textCase(nil)
+                                .foregroundColor(Color.gray)
+                                .opacity(0.7)
+                        }
+                        .frame(maxWidth: .infinity, alignment: .center)
                     }
-                    .padding([.top, .bottom], 20)
-                    .frame(maxWidth: .infinity, alignment: .center)
                 }
+                .padding([.top, .bottom], 20)
 
                 PhotosPicker(selection: $photoItem, matching: .any(of: [.images, .not(.videos)])) {
                     Text(tempStrip == nil ? "Select a Strip Image" : "Change Strip Image")
@@ -68,20 +56,13 @@ struct CustomizeStripImage: View {
                 .onChange(of: photoItem) {
                     Task {
                         if let loaded = try? await photoItem?.loadTransferable(type: Data.self) {
-                            tempStrip = UIImage(data: loaded)!
+                            var image = UIImage(data: loaded)!
+                            image = cropToStripAspectRatio(image) ?? image
+                            tempStrip = image
                         } else {
                             print("Failed")
                         }
                     }
-                }
-
-                if offsetBound > 1 { // if the selected strip image is already the correct aspect ratio, don't show the adjustment slider
-                    HStack {
-                        Text("Vertical Offset")
-                        Slider(value: $cropOffset, in: -offsetBound ... offsetBound, step: 1)
-                    }
-                    .padding(14)
-                    .listSectionBackgroundModifier()
                 }
 
                 if tempStrip != nil {
@@ -109,17 +90,10 @@ struct CustomizeStripImage: View {
 
                 ToolbarItem(placement: .confirmationAction) {
                     Button("Save", systemImage: "checkmark") {
-                        if let strip = tempStrip {
-                            let imageWidth = PassKitConstants.StripImage.width
-                            let imageHeight = PassKitConstants.StripImage.height
-                            let scaledCropOffset = cropOffset * imageHeight / size.height
-
-                            // Store the largest (3x) generated strip image
-                            passObject.stripImage = ImageRenderer(content:
-                                OffsetCroppedStripImage(cropOffset: scaledCropOffset * 3, strip: strip)
-                                    .frame(width: imageWidth * 3, height: imageHeight * 3)
-                            ).uiImage?.pngData() ?? Data()
-
+                        if let strip = tempStrip,
+                           let cropped = cropToStripAspectRatio(strip)
+                        {
+                            passObject.stripImage = cropped.pngData() ?? Data()
                             // Remove background image (incompatible with strip image)
                             passObject.backgroundImage = Data()
                         }
@@ -141,4 +115,26 @@ struct CustomizeStripImage: View {
 
 #Preview {
     CustomizeStripImage(passObject: .constant(MockModelData().passObjects[0]))
+}
+
+private func cropToStripAspectRatio(_ image: UIImage) -> UIImage? {
+    guard let cgImage = image.cgImage else { return nil }
+
+    let srcW = CGFloat(cgImage.width)
+    let srcH = CGFloat(cgImage.height)
+    let targetAspect = PassKitConstants.StripImage.aspectRatio
+
+    let cropRect: CGRect
+    if srcW / srcH > targetAspect {
+        // Image is wider than target: crop width, center horizontally
+        let cropW = srcH * targetAspect
+        cropRect = CGRect(x: (srcW - cropW) / 2, y: 0, width: cropW, height: srcH)
+    } else {
+        // Image is taller than target: crop height, center vertically
+        let cropH = srcW / targetAspect
+        cropRect = CGRect(x: 0, y: (srcH - cropH) / 2, width: srcW, height: cropH)
+    }
+
+    guard let cropped = cgImage.cropping(to: cropRect) else { return nil }
+    return UIImage(cgImage: cropped, scale: image.scale, orientation: image.imageOrientation)
 }
