df8d46d99d6b92dc911fec85932b23d6d044d162
Andrew Hanshaw <drew@andrewhanshaw.com>
2026-07-15 23:51:40 -0500

    WIP use NavigationSplitView for main ContentView

--- PassKeepr/Views/PassGridView.swift
+++ PassKeepr/Views/PassGridView.swift
@@ -14,7 +14,7 @@ struct PassGridView: View {
     @EnvironmentObject var modelData: ModelData
 
     @Binding var importedPassURL: URL?
-    var columnCount: Int
+    @State private var columnCount: Int = 2
 
     @Namespace private var zoomNamespace
 
@@ -179,6 +179,11 @@ struct PassGridView: View {
                 }
             }
         } // ZStack
+        .onGeometryChange(for: Bool.self) { proxy in
+            proxy.size.width > proxy.size.height
+        } action: { isLandscape in
+            columnCount = isLandscape ? 4 : 2
+        }
         .navigationDestination(for: UUID.self) { id in
             if let index = modelData.passObjects.firstIndex(where: { $0.id == id }) {
                 EditPass(objectToEdit: $modelData.passObjects[index], isNewPass: false)
