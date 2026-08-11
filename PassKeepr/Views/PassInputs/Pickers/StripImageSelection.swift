23a0c9461d9566021718c474c025b8f5eee7e9c6
Andrew Hanshaw <drew@andrewhanshaw.com>
2026-08-10 21:47:29 -0500

    WIP Allow strip images on passes with QR codes
    WIP because we need to confirm whether this is new behavior or not

--- PassKeepr/Views/PassInputs/Pickers/StripImageSelection.swift
+++ PassKeepr/Views/PassInputs/Pickers/StripImageSelection.swift
@@ -8,15 +8,8 @@ struct StripImageSelection: View {
     var body: some View {
         VStack(alignment: .leading) {
             Toggle("Strip Image", isOn: $passObject.isCustomStripImageOn)
-                .foregroundColor(passObject.barcodeType == .qr ? .secondary : .primary)
                 .padding(14)
                 .listSectionBackgroundModifier()
-                .disabled(disableControl || passObject.barcodeType == .qr)
-                .onChange(of: passObject.barcodeType) {
-                    if passObject.barcodeType == .qr {
-                        passObject.isCustomStripImageOn = false
-                    }
-                }
                 .onChange(of: passObject.isCustomStripImageOn) {
                     // When the strip image is on, the auxiliary fields share space with the secondary fields.
                     // If the strip image is on (either by the user turning it on or by importing a pass with a strip image)
@@ -28,17 +21,6 @@ struct StripImageSelection: View {
                         passObject.isAuxiliaryFieldOneOn = false
                     }
                 }
-                .onAppear {
-                    if passObject.barcodeType == .qr {
-                        passObject.isCustomStripImageOn = false
-                    }
-                }
-            if passObject.barcodeType == .qr {
-                Text("Strip images are not compatible with QR codes")
-                    .font(.footnote)
-                    .foregroundColor(.secondary)
-                    .padding(.leading, 14)
-            }
         }
     }
 }
